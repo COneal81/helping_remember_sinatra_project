2c91e5e69066b2574c14d4dfe395c38f9e48af6f
github username <github email address>
2019-09-19 11:46:57 -0400

    Installed gem sinatra flash, required in application_controller, placed code in layout.erb, just above yield

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,10 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
 
     enable :sessions
+    require 'sinatra/flash'
     set :session_secret, "za7dw4n2dlr3sockw5fkedj3mLgoemw04825jo6ii654jd9fjw0"
+    
+    
   end
 
   get "/" do
