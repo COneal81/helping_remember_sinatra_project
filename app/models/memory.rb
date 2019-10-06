40f1b7d11fb71311dc61d69dbc61d2c85612c225
github username <github email address>
2019-10-06 16:15:18 -0400

    Reverted changes to memories controller post memories for a better users experience.  also commented in memory rb for future changes to validations

--- app/models/memory.rb
+++ app/models/memory.rb
@@ -3,6 +3,10 @@ class Memory < ActiveRecord::Base
     belongs_to :category
 
     # impliment validators
+    #FUTURE :  If the validators are changed, check the 
+    # memories_controller - post/memories flash error message.  
+    # This is hard coded for an easier user experience.
+
     validates :title, :description, :date, presence: true
 end
 
